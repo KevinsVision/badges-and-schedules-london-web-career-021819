86cbb0c1a8e202235eb997d77602cffd65267bb8
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-01-30 05:16:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(rooms)
   #rooms = 7
   #names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  rooms.each do |person|
+  rooms.each_with_index do |person, rooms|
   puts "Hello, #{person}! You'll be assigned to room #{rooms}!"
  # apples_taken_out += 1
 end
