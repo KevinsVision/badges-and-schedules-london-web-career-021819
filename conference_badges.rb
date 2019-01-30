b515b57854ab7ab951f77fa4ec0dbee228ba57ad
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-01-30 05:08:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+
+def badge_maker(name)
+   return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  person = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  attendees.map do |person|
+ "Hello, my name is #{person}."
+end
+end
+
+def assign_rooms(rooms)
+  #rooms = 7
+  #names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  rooms.each do |person|
+  puts "Hello, #{person}! You'll be assigned to room #{rooms}!"
+ # apples_taken_out += 1
+end
+end
